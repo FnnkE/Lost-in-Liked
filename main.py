76cbc9c2eabcdc4d24ad4a89a6038b44b5fc2591
FnnkE <efunk2@gmu.edu>
2023-03-16 02:12:55 -0400

    Implement Base of Spotipy
    Changed the name of the playlist. Most importantly started cleaning code. WIP

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import base64
 from requests import post, get, delete
 import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
@@ -10,91 +12,55 @@ clientID = os.getenv("CLIENT_ID")
 clientSecret = os.getenv("CLIENT_SECRET")
 TOKEN = os.getenv("TOKEN")
 
-def getToken():
-    authString = clientID + ":" + clientSecret
-    authBytes = authString.encode("utf-8")
-    authBase64 = str(base64.b64encode(authBytes), "utf-8")
 
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + authBase64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    jsonResult = json.loads(result.content)
-    token = jsonResult["access_token"]
-    return token
+scope = "ugc-image-upload, user-read-playback-state, user-modify-playback-state, user-read-currently-playing, app-remote-control, streaming, playlist-read-private, playlist-read-collaborative, playlist-modify-private, playlist-modify-public, user-follow-modify, user-follow-read, user-read-playback-position, user-top-read, user-read-recently-played, user-library-modify, user-library-read, user-read-email, user-read-private"
+auth_manager = SpotifyOAuth(client_id=clientID, client_secret=clientSecret, redirect_uri='http://lost-in-like.com', scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-def getAuthHeader(token):
-    return {"Authorization": "Bearer " + token}
-
-def getPlaylists(token, username):
+def getPlaylists():
     flag = True
     playlists = []
     offset = 0
     while flag:
-        url=f"https://api.spotify.com/v1/users/{username}/playlists?limit=50&offset={offset}"
-        headers=getAuthHeader(token)
-        result = get(url, headers=headers)
-        jsonResult = json.loads(result.content)["items"]
-        if (len(jsonResult)!=50):
+        playlists = sp.current_user_playlists(limit=50, offset=offset)
+        if (len(playlists)!=50):
             flag = False
         offset += 50
-        playlists.extend(jsonResult)
+        playlists.extend(playlists) #limit output to only items?
         print("Getting playlists: ", offset)
     return playlists
 
-def getPlaylistSongs(token, playlistID):
+def getPlaylistSongs(playlistID):
     flag = True
     songs = []
     offset = 0
     while flag:
-        url = f"https://api.spotify.com/v1/playlists/{playlistID}/tracks?offset={offset}"
-        headers = getAuthHeader(token)
-        result = get(url, headers=headers)
-        jsonResult = json.loads(result.content)["items"]
-        if (len(jsonResult)!=100):
+        tracks = sp.playlist_tracks(playlistID, fields=None, limit=100, offset=offset, market=None, additional_types=('track', )) #type and market?
+        if (len(tracks)!=100):
             flag = False
         offset += 100
-        songs.extend(jsonResult)
+        songs.extend(tracks)
     return songs
 
-def createPlaylist(token, username):
-    url = f"https://api.spotify.com/v1/users/{username}/playlists"
-    headers = {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json"
-        }
-    data = {
-        "name": "Lost in Liked™",
-        "description": "Being lost is worth the being found | https://github.com/FnnkE/Lost-in-Liked",
-        "public": "true"
-        }
-    result = post(url, headers=headers, data=json.dumps(data))
-    return json.loads(result.content)['id']
+def createPlaylist(username):
+    result = sp.user_playlist_create(username, '<TEST/> Lost in Liked', public=True, collaborative=False, description='Being lost is worth the being found | https://github.com/FnnkE/Lost-in-Liked')
+    #what does result output?
+    return result #json.loads(result.content)['id']
 
-def addSongs(playlistID, uris, token):
-    url = f"https://api.spotify.com/v1/playlists/{playlistID}/tracks?uris=" + uris
-    headers = {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json"
-        }
-    result = post(url, headers=headers)
+def addSongs(playlistID, uris):
+    result = sp.playlist_add_items(playlistID, uris, position=None) # None?
+    return result # Output?
 
 def getLikedSongs(token):
     flag = True
     songs = []
     offset = 0
     while flag:
-        url=f"https://api.spotify.com/v1/me/tracks?limit=50&offset={offset}"
-        headers=getAuthHeader(token)
-        result = get(url, headers=headers)
-        jsonResult = json.loads(result.content)["items"]
-        if (len(jsonResult)!=50):
+        result = sp.current_user_saved_tracks(limit=50, offset=offset, market=None) #market?
+        if (len(result)!=50):
             flag = False
         offset += 50
-        songs.extend(jsonResult)
+        songs.extend(result)
         print("Getting Liked Songs: ", offset)
     return songs
 
@@ -118,7 +84,6 @@ def removeSongs(playlistID, token, songs):
     return result
 
 #Create user token
-token = TOKEN#getToken()
 username = 'nerd-e'
 
 #Get user data
@@ -129,7 +94,7 @@ result = getPlaylists(token, username)
 #Playlist setup
 playlistMade = False
 for playlist in result:
-    if playlist["name"] == "Lost in Liked™":
+    if playlist["name"] == "</> Lost in Liked":
         playlistMade = True
         newPlaylistID = playlist['id']
         print(f'Found Playlist...{newPlaylistID}')
@@ -180,3 +145,4 @@ for idx, song in enumerate(likedURIs):
 uris = uris[:-1]
 addSongs(newPlaylistID, uris, token)
 print('DONE!')
+"""
